23d804244a64d5e2d3a998c79e5b2e0d4579ac45
sunny.chao <sunny.chao@deutschinc.com>
2023-11-04 23:03:03 -0400

    made error go away

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,23 @@ import UserInterface from '../components/UserInterface'
 import styles from '../styles/Home.module.css';
 
 
-const GameContext = createContext();
+export const GameContext = createContext();
 
 export default function Home() {
 	const [misses, setMisses] = useState(0);
 
     function missedShot(event) {
-        console.log(event.target.id);
+        console.log("missed click");
         setMisses((prevState) => prevState + 1);
     }
 	
 	return (
 		<div className={styles.container}>
 			<div className={styles.game_and_ui_container}>
-				<GameContext.Provider value={{ misses }}>
+				<GameContext.Provider value={{misses}}>
 					<GameContainer onClick={(e) => missedShot(e)}/>
+					<UserInterface />
 				</GameContext.Provider>
-				<UserInterface />
 			</div>
 		</div>
 	);
