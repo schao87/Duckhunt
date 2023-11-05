94276b17fa5a37d5ba5071d5c53f00ac178eef92
Noximus <hrichardson83@gmail.com>
2023-11-04 20:47:13 -0400

    fixed error

--- components/UserInterface.js
+++ components/UserInterface.js
@@ -1,32 +1,23 @@
 'use client';
 import styles from './UserInterface.module.scss';
-import { useState, useEffect } from 'react';
-import {gsap} from 'gsap';
+import { useState, useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 import { Draggable } from 'gsap/dist/all';
-gsap.registerPlugin(Draggable) 
+gsap.registerPlugin(Draggable);
 
+export default function UserInterface() {
+  const [misses, setMisses] = useState(0);
 
-export default function UserInterface(){
-    const [misses, setMisses] = useState(0)
-    const gamecontainer = document.getElementById('gamecontainer');
-    gamecontainer.addEventListener("click", missedShot)
+  function missedShot(event) {
+    console.log('event.target.id');
+    setMisses((prevState) => prevState + 1);
+  }
 
-    function missedShot(event){
-        console.log(event.target.id)
-        setMisses(prevState => prevState + 1)
-    }
-
-    useEffect(() => {
-
-    },[])
-
-
-
-    return(
-        <div id="userinterface" className={styles.userinterface}>
-            <div id="misscounter" className={styles.miss_counter}>
-                {misses}
-            </div>
-        </div>
-    )
+  return (
+    <div id="userinterface" className={styles.userinterface}>
+      <div id="misscounter" onClick={(e) => missedShot(e)} className={styles.miss_counter}>
+        {misses}
+      </div>
+    </div>
+  );
 }
