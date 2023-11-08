da8ca346c5eb8d55d18489ca49642647725a3b7d
sunny.chao <sunny.chao@deutschinc.com>
2023-11-08 12:02:56 -0500

    building logic for random spawning around game container

--- components/Duck.js
+++ components/Duck.js
@@ -7,18 +7,18 @@ import {gsap} from 'gsap';
 
 export default function Duck(){
     const duckRef = useRef();
-    
-    let randomX = ""
-    let randomY = ""
-    let randomSpawnPositions = ["x:-50", "x:800", "y:-50", "y:480" ]
+    const [duckPosition, setDuckPosition] = useState('')
+    const [randomX, setRandomX] = useState()
+    const [randomY, setRandomY] = useState()
+   
 
     function getRandomCoordinate() {
         // Get random x and y coordinates within the div's width and height
         const x = Math.floor(Math.random() * 800); // Random x coordinate between 0 and 800
         const y = Math.floor(Math.random() * 480); // Random y coordinate between 0 and 480
         
-        randomX = x 
-        randomY = y
+        return setRandomX(x), setRandomY(y) 
+    
     }
     function flightTime(){
         //Get random number between 2 and 5
@@ -26,21 +26,68 @@ export default function Duck(){
         return timeOfFlight
     }
 
+    function positionDuck() {
+        const gameContainerDiv = document.querySelector('.gamecontainer');
+        const Duck = document.querySelector('.duck');
+        
+        // Calculate gameContainer dimensions
+        const gameContainerWidth = gameContainerDiv.clientWidth;
+        const gameContainerHeight = gameContainerDiv.clientHeight;
+      
+        // Calculate duck dimensions
+        const duckWidth = Duck.clientWidth;
+        const duckHeight = Duck.clientHeight;
+      
+        // Determine random position and side
+        const sides = ['above', 'below', 'left', 'right'];
+        const randomSide = sides[Math.floor(Math.random() * sides.length)];
+        setDuckPosition(randomSide)
+        
+        let randomXPos, randomYPos;
+      
+        // Set random positions based on the selected side
+        if (randomSide === 'above') {
+          randomXPos = Math.floor(Math.random() * (gameContainerWidth - duckWidth));
+          randomYPos = -duckHeight;
+        } else if (randomSide === 'below') {
+          randomXPos = Math.floor(Math.random() * (gameContainerWidth - duckWidth));
+          randomYPos = gameContainerHeight;
+        } else if (randomSide === 'left') {
+          randomXPos = -duckWidth;
+          randomYPos = Math.floor(Math.random() * (gameContainerHeight - duckHeight));
+        } else if (randomSide === 'right') {
+          randomXPos = gameContainerWidth;
+          randomYPos = Math.floor(Math.random() * (gameContainerHeight - duckHeight));
+        }
+      
+        // Set the duck's position
+        Duck.style.left = randomXPos + 'px';
+        Duck.style.top = randomYPos + 'px';
+      }
 
     useLayoutEffect(() => {
         let duck = document.querySelector(".duck")
 
         let ctx = gsap.context((context) => {
             getRandomCoordinate()
-
+            positionDuck()
+            
+            if(duckPosition === 'left'){
+                gsap.to(".duck", {duration:flightTime(), x: randomX, y: randomY})
+            }else if(duckPosition === 'right'){
+                gsap.to(".duck", {duration:flightTime(), x: randomX, y: randomY})
+            }else if(duckPosition === 'above'){
+                gsap.to(".duck", {duration:flightTime(), x: randomX, y: randomY})
+            }else{
+                gsap.to(".duck", {duration:flightTime(), x: randomX, y: -randomY})
+            }
+            
+            
             context.add("duckShot",() => {
                 ctx.kill()
                 gsap.to(".duck", {duration:.5, y: 500})
                 
             })
-
-            gsap.to(".duck", {duration:flightTime(), x: randomX, y: randomY})
-
         }); // <-- scope (for selector text - only find descendants of this)
 
         duck.addEventListener("click", (e) => {
@@ -50,7 +97,11 @@ export default function Duck(){
         return () => ctx.revert(); // <-- CLEANUP!
     }, []);
 
-
+    useEffect(() => {
+       console.log(duckPosition)
+       console.log(randomX)
+       console.log(randomY)
+    },[duckPosition])
 
     return(
         <div ref={duckRef} className={`${styles.duck} duck`}>
